1f977e243291e9afceacdbad24eb74c5bae5e610
cheeeeze <nyj9349@hanmail.net>
2021-11-30 21:56:29 +0900

    inital commit

---
+++ frontend/pages/about/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Hello world!</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  fetch() {
+    console.log( process );
+  },
+  methods : {
+
+  },
+}
+</script>
