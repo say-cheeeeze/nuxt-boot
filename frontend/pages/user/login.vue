706cae0e2b31225766b8d73c602cc1bcf57a936a
cheeeeze <nyj9349@hanmail.net>
2021-11-30 22:48:46 +0900

    add login form

--- frontend/pages/user/login.vue
+++ frontend/pages/user/login.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
 	  <h1>login page</h1>
+	  <div>
+		  <span>ID</span><el-input placeholder="ID 를 입력하세요" v-model="inputId"></el-input>
+	  </div>
+	  <div>
+		  <span>PW</span><el-input placeholder="비밀번호를 입력하세요" v-model="inputPwd"></el-input>
+	  </div>
+
   </div>
 </template>
 
@@ -8,7 +15,10 @@
 export default {
 	data() {
 		return {
-		
+			// 입력받은 id
+			inputId : "",
+			// 입력받은 pwd
+			inputPwd : "",
 		}
 	}
 }
